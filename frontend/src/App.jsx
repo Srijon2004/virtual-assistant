e52f2f986213c060b5ae8fdc62dfe85297ebc022
Srijon2004 <srijonchowdhury6@gmail.com>
2026-01-08 19:21:38 +0530

    Updated frontend code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,6 +25,59 @@
 
 
 
+// import React, { useContext } from 'react';
+// import { Navigate, Route, Routes } from 'react-router-dom';
+// import SignUp from './pages/SignUp';
+// import SignIn from './pages/SignIn';
+// import Customize from './pages/Customize';
+// import { userDataContext } from './context/UserContext';
+// import Home from './pages/Home';
+// import Customize2 from './pages/Customize2';
+
+// function App() {
+//   const { userData } = useContext(userDataContext);
+//   return (
+//     <Routes>
+//       <Route
+//         path="/"
+//         element={
+//           userData?.assistantImage && userData?.assistantName ? (
+//             <Home />
+//           ) : (
+//             <Navigate to={'/customize'} />
+//           )
+//         }
+//       />
+//       <Route
+//         path="/signup"
+//         element={!userData ? <SignUp /> : <Navigate to={'/'} />}
+//       />
+//       <Route
+//         path="/signin"
+//         element={!userData ? <SignIn /> : <Navigate to={'/'} />}
+//       />
+//       <Route
+//         path="/customize"
+//         element={userData ? <Customize /> : <Navigate to={'/signup'} />}
+//       />
+//       <Route
+//         path="/customize2"
+//         element={userData ? <Customize2 /> : <Navigate to={'/signup'} />}
+//       />
+//     </Routes>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
 import React, { useContext } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp';
@@ -36,33 +89,42 @@ import Customize2 from './pages/Customize2';
 
 function App() {
   const { userData } = useContext(userDataContext);
+
   return (
     <Routes>
       <Route
         path="/"
         element={
-          userData?.assistantImage && userData?.assistantName ? (
-            <Home />
+          userData ? (
+            userData?.assistantImage && userData?.assistantName ? (
+              <Home />
+            ) : (
+              <Navigate to={'/customize'} />
+            )
           ) : (
-            <Navigate to={'/customize'} />
+            <Navigate to={'/signin'} /> // Change fallback to signin
           )
         }
       />
+      
       <Route
         path="/signup"
         element={!userData ? <SignUp /> : <Navigate to={'/'} />}
       />
+      
       <Route
         path="/signin"
         element={!userData ? <SignIn /> : <Navigate to={'/'} />}
       />
+      
       <Route
         path="/customize"
-        element={userData ? <Customize /> : <Navigate to={'/signup'} />}
+        element={userData ? <Customize /> : <Navigate to={'/signin'} />} // Change to signin
       />
+      
       <Route
         path="/customize2"
-        element={userData ? <Customize2 /> : <Navigate to={'/signup'} />}
+        element={userData ? <Customize2 /> : <Navigate to={'/signin'} />} // Change to signin
       />
     </Routes>
   );
