9fd912ed12b6ed458698d6affa0bb77cf13afcef
Srijon2004 <srijonchowdhury6@gmail.com>
2026-01-10 00:30:41 +0530

    Updated few  code

--- frontend/src/pages/Customize.jsx
+++ frontend/src/pages/Customize.jsx
@@ -26,13 +26,20 @@ setFrontendImage(URL.createObjectURL(file))
         <MdKeyboardBackspace className='absolute top-[30px] left-[30px] text-white cursor-pointer w-[25px] h-[25px]' onClick={()=>navigate("/")}/>
         <h1 className='text-white mb-[40px] text-[30px] text-center '>Select your <span className='text-blue-200'>Assistant Image</span></h1>
         <div className='w-full max-w-[900px] flex justify-center items-center flex-wrap gap-[15px]'>
-      <Card image={image1}/>
+      {/* <Card image={image1}/>
        <Card image={image2}/>
         <Card image={image3}/>
          <Card image={image4}/>
           <Card image={image5}/>
-           <Card image={image6}/>
-            <Card image={image7}/>
+           <Card image={image6}/> */}
+            {/* <Card image={image7}/> */}
+      <Card image="/assistant/image1.png"/>
+      <Card image="/assistant/image2.jpg"/>
+      <Card image="/assistant/image3.png"/>
+      <Card image="/assistant/image4.png"/>
+      <Card image="/assistant/image5.png"/>
+      <Card image="/assistant/image6.jpeg"/>
+      <Card image="/assistant/image7.jpeg"/>
      <div className={`w-[70px] h-[140px] lg:w-[150px] lg:h-[250px] bg-[#020220] border-2 border-[#0000ff66] rounded-2xl overflow-hidden hover:shadow-2xl hover:shadow-blue-950 cursor-pointer hover:border-4 hover:border-white flex items-center justify-center ${selectedImage=="input"?"border-4 border-white shadow-2xl shadow-blue-950 ":null}` } onClick={()=>{
         inputImage.current.click()
         setSelectedImage("input")
